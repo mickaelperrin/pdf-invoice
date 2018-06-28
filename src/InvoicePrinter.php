b5563d29df51c7b75d8a25607e435896414fd9e8
Mickaël PERRIN <dev@mickaelperrin.fr>
2018-06-28 19:06:29 +0200

    [FEATURE] Manage text charset conversion for footer note

--- src/InvoicePrinter.php
+++ src/InvoicePrinter.php
@@ -629,7 +629,7 @@ class InvoicePrinter extends FPDF
         $this->SetY(-$this->margins['t']);
         $this->SetFont($this->font, '', 8);
         $this->SetTextColor(50, 50, 50);
-        $this->Cell(0, 10, $this->footernote, 0, 0, 'L');
+        $this->Cell(0, 10, iconv("UTF-8", $this->charset,  $this->footernote), 0, 0, 'L');
         $this->Cell(0, 10, $this->lang['page'] . ' ' . $this->PageNo() . ' ' . $this->lang['page_of'] . ' {nb}', 0, 0,
             'R');
     }
